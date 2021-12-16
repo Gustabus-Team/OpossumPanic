2f1cdfd18562e48c16f1224a1d91b1172ab20a5c
boxdevvvv <carlos.llanquinao.r@gmail.com>
2021-12-15 22:29:12 -0300

    canvas

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -6,19 +6,21 @@ using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
+    public RectTransform cargandoPantalla;
     public RectTransform PanelConSkins;
     public RectTransform panelTienda;
     public Button skinsBoton;
     public Button tiendaBoton;
-    void Start()
+    private void Awake()
     {
-        
+        cargandoPantalla.gameObject.SetActive(true);
+        StartCoroutine(cargando());
     }
-
-    // Update is called once per frame
-    void Update()
+    IEnumerator cargando()
     {
-        
+        yield return new WaitForSeconds(1);
+        cargandoPantalla.DOAnchorPosY(2000, 0.5f);
+        yield break;
     }
 
     public void BotonSkinsPresionado()
@@ -27,17 +29,19 @@ public class UIManager : MonoBehaviour
         tiendaBoton.interactable = false;
         PanelConSkins.DOAnchorPosX(0, 0.5f);
     }
+    public void BotonTiendaPresionado()
+    {
+        skinsBoton.interactable = false;
+        tiendaBoton.interactable = false;
+        panelTienda.DOAnchorPosX(0, 0.5f);
+    }
     public void BotonVolverPresionado()
     {
         skinsBoton.interactable = true;
         tiendaBoton.interactable = true;
         PanelConSkins.DOAnchorPosX(-2000, 0.5f);
+        panelTienda.DOAnchorPosX(2000, 0.5f);
     }
-    public void BotonTiendaPresionado()
-    {
-        skinsBoton.interactable = false;
-        tiendaBoton.interactable = false;
-        PanelConSkins.DOAnchorPosX(0, 0.5f);
-    }
+   
 
 }
