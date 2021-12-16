c5bb35c50754649049f412cd337b412bf91caf0b
boxdevvvv <carlos.llanquinao.r@gmail.com>
2021-12-16 06:57:34 -0300

    cosas

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,38 +6,51 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     /// Salto de Zarigueya
-    public float alturaTorreZaris = 1.2f;
-    
-    
+    public float alturaTorreZaris = 1.2f;   
     //2.3f;// 1.2f;
     /// </summary>
-
-
-
     public static GameManager _gameManager;
     public int dineroActual = 1000;
     public static int dineroTotal;
     public int NumeroDeZarigueyasEncima;
 
-    
+    public TextMeshProUGUI actualDinero;
     private void Awake()
     {
         _gameManager = this;
     }
-    void Start()
+    public void finalizaNivel()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
+    private void Start()
+    {
+        currentTime = startingTime;
+    }
     void Update()
     {
-        
+        actualDinero.text = dineroTotal.ToString();
+        //timer
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
     }
 
+    float currentTime = 0f;
+    float startingTime = 30;
 
+    public TextMeshProUGUI countdownText;
 
-
-
-
+   
+    
 }
+
+
+
+
+
+
