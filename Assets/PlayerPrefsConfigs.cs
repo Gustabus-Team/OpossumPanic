ef05171f83fbbf934545148633fcde9444b2a94b
boxdevvvv <carlos.llanquinao.r@gmail.com>
2021-12-15 22:47:19 -0300

    jhkhj

---
+++ Assets/PlayerPrefsConfigs.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefsConfigs : MonoBehaviour
+{
+    public Button[] skins;
+
+    private void Start()
+    {
+        
+    }
+
+
+
+
+
+}
